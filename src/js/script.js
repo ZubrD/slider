ec054dbca7551f89bcd2e0d05d899806d0a4c272
Den Zubr <fort_zd@mail.ru>
2021-11-25 13:13:06 +0700

    Первоначальное размещение элементов слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,38 @@
 import { Ranger, Interval, Button, Input } from './view.js'
-import {func_alert} from './func_prim_1.js'
 
 window.onload = sliderInit ()
 
 function sliderInit () {
-  // func_alert()
   let elements = document.querySelectorAll('.zdslider');
   if ( elements.length != 0 ) {
+    
+
+
     let counter = 1;
     let i = 0;         /*  Счётчик цикла для опр-я номера ranger в массиве */
     for ( let elem of elements ) {
         let ranger = new Ranger();
         ranger.appendTo(elem)
         ranger.setAttribute ('id', 'r_' + counter);
+        ranger.setAttribute ('data-inst', counter);
+        ranger.setAttribute ('data-type', 'ranger');
 
         let interval = new Interval();
         let ranger_div = document.querySelectorAll('.ranger')[i]
         interval.setAttribute('id', 'int_' + counter);
+        interval.setAttribute('data-inst', counter);
+        interval.setAttribute('data-type', 'interval');
         interval.appendTo(ranger_div)
 
         let button_1 = new Button();
         let button_2 = new Button();
 
         button_1.setAttribute('id', 'b1_' + counter);
+        button_1.setAttribute('data-type', 'btn-first');
+        button_1.setAttribute('data-inst', counter);
         button_2.setAttribute('id', 'b2_' + counter);
+        button_2.setAttribute('data-type', 'btn-second');
+        button_2.setAttribute('data-inst', counter);
 
         button_1.appendTo(ranger_div);
         button_2.appendTo(ranger_div);
@@ -39,10 +48,68 @@ function sliderInit () {
 
         counter ++;
         i ++;
-    }   
-  } else {
-      alert ('Нужно добавить div с классом zdslider');
+    
     }
+    counter = 0
+    for (let elem of elements) {
+      let ranger = document.querySelectorAll('.ranger')[counter]
+      let interval = document.querySelectorAll('.ranger__interval')[counter]
+      let button_1 = document.querySelectorAll('[data-type="btn-first"]')[counter]
+      let button_2 = document.querySelectorAll('[data-type="btn-second"]')[counter]
 
+      let ranger_coords = getCoords(ranger)
+      button_1.style.marginLeft = '0px';
+      button_2.style.marginLeft = (ranger.offsetWidth-button_1.offsetWidth) + 'px';
+      interval.style.width = (ranger.offsetWidth-button_1.offsetWidth) + 'px';
+      console.log(ranger_coords)
+      counter ++
+    }   
+  }
+  
+
+
+
+  // clickBtn()
   
 }
+
+function clickBtn () {
+
+  document.onmousedown = function (event) {
+    let elem = event.target.dataset.inst    /* Определяю номер текущего слайдера (если их несколько) */
+    
+    let slider_block = document.querySelectorAll('[data-inst="'+elem+'"]')
+    let ranger
+    let interval
+    let button_1
+    let button_2
+    for (let el of slider_block){
+      if (el.dataset.type === 'ranger') {
+        ranger = el      
+      }
+      if (el.dataset.type === 'interval') {
+        interval = el
+      }
+      if (el.dataset.type === 'btn-first') {
+        button_1 = el
+      }
+      if (el.dataset.type === 'btn-second') {
+        button_2 = el
+      }            
+    }
+
+
+    let ranger_coords = getCoords(ranger)
+    button_1.style.marginLeft = '0px';
+    button_2.style.marginLeft = (ranger.offsetWidth-button1.offsetWidth) + 'px';
+    between.style.width = (slider.offsetWidth-button1.offsetWidth) + 'px'; 
+  }
+}
+
+function getCoords(elem) {
+  let coords = elem.getBoundingClientRect();
+  return {
+      top: coords.top,
+      left: coords.left
+  };
+}  
--- src/js/view.js
+++ src/js/view.js
@@ -2,6 +2,7 @@ export class Ranger {
     constructor (options) {
         this.$el = document.createElement('div')
         this.$el.classList.add('ranger')
+
     }
     appendTo(parent) {
         parent.appendChild(this.$el)
@@ -34,6 +35,8 @@ export class Button {
     constructor (options) {
         this.$el = document.createElement('button')
         this.$el.classList.add('ranger__button')
+
+        this.#setup()
     }
     appendTo(parent) {
         parent.appendChild(this.$el)
@@ -41,6 +44,32 @@ export class Button {
     setAttribute(attr, number) {
         this.$el.setAttribute(attr, number)
     }
+
+    #setup() {
+        this.clickHandler = this.clickHandler.bind(this)
+        this.$el.addEventListener('click', this.clickHandler)
+        this.$slider = document.querySelector('.ranger')
+        this.$button = document.querySelector('[id="b1_1"]')
+        // this.$el.addEventListener('mousedown', this.getCoords)
+    }
+    clickHandler (event) {
+    //    console.log(event.target.id)
+       const {type} = event.target.dataset
+        if (type === 'btn-first') {
+            // console.log('Первая кнопка', event.target.id, this.$button)
+            
+        } else if (type === 'btn-second') {
+            // console.log('Вторая кнопка', event.target.id, this.$button_2)
+            this.getCoords(this.$slider)
+        }
+    }
+    getCoords(elem) {
+        let coords = elem.getBoundingClientRect();
+        return {
+            top: coords.top,
+            left: coords.left
+        };
+    }  
 }
 
 export class Input {
