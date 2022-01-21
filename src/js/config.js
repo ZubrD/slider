fa36990697c0556d3873a674fe15df346b2fb10c
Den Zubr <fort_zd@mail.ru>
2022-01-21 13:27:36 +0700

    Перевёл на typescript config.ts

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,10 @@
 export class Config {
-    runner_number = 2
-    min = 10
-    max = 150
-    discrete = 'no'
-    // orientation= 'vertical'
-    orientation= 'horizontal'
+    constructor() {
+        this.runner_number = 2;
+        this.min = 10;
+        this.max = 150;
+        this.discrete = 'no';
+        // orientation: string = 'vertical'
+        this.orientation = 'horizontal';
+    }
 }
---
+++ src/ts/config.ts
@@ -0,0 +1,8 @@
+export class Config {
+    runner_number: number = 2
+    min: number = 10
+    max: number = 150
+    discrete: string = 'no'
+    // orientation: string = 'vertical'
+    orientation: string = 'horizontal'
+}
