95314368f09892f47042fa056e59faccfc485778
Den Zubr <fort_zd@mail.ru>
2022-02-16 16:17:58 +0700

    Создал readme.md

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export let configObj = {
     runner_number: 2,
-    min: 10,
+    min: 12,
     max: 150,
     discrete: 'no',
     // orientation: 'vertical'
--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,6 +1,6 @@
 export let configObj = {
     runner_number: 2 as number,
-    min: 10 as number,
+    min: 12 as number,
     max: 150 as number,
     discrete: 'no' as string,
     // orientation: 'vertical'
