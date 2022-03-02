712b02c7fb451f2721a94a22dc8f7ce04dcfd57d
Den Zubr <fort_zd@mail.ru>
2022-03-02 15:17:06 +0700

    Рефакторинг tipToggler.ts по требованиям AirBnb

--- src/js/tipToggler.js
+++ src/js/tipToggler.js
@@ -2,50 +2,51 @@ export function showTip(elem, orientation) {
     let parent = elem.parentNode.parentNode.querySelector('.zdslider');
     let parentRanger = parent.querySelector('.ranger');
     let buttons = parentRanger.querySelectorAll('.ranger__button');
-    for (let elem of buttons) {
-        if (orientation == 'horizontal') {
+    buttons.forEach((elem) => {
+        if (orientation === 'horizontal') {
             elem.classList.add('ranger__button-tip');
         }
-        else if (orientation == 'vertical') {
+        else if (orientation === 'vertical') {
             elem.classList.add('ranger-vert__button-tip');
         }
-    }
+    });
 }
 export function hideTip(elem) {
     let parent = elem.parentNode.parentNode.querySelector('.zdslider');
     let tip = elem.parentNode.querySelector('.zdslider-panel__check-tip');
-    tip.checked = false; /* Сбрасываю флаг надписи */
+    /* Сбрасываю флаг надписи */
+    tip.checked = false;
     let parentRanger = parent.querySelector('.ranger');
     let buttons = parentRanger.querySelectorAll('.ranger__button');
-    for (let elem of buttons) { /* Удаляю стили ярлыков */
+    /* Удаляю стили ярлыков */
+    buttons.forEach((elem) => {
         elem.classList.remove('ranger__button-tip');
         elem.classList.remove('ranger-vert__button-tip');
-    }
+    });
 }
 export function forTip(target, coord) {
     const config = target.parentNode.parentNode.parentNode.querySelector('.zdslider-config');
     const configMin = Number(config.dataset.min);
     const configMax = Number(config.dataset.max);
-    // const ranger_height: number = ( target.parentNode as HTMLElement).offsetHeight
-    // const ranger_width: number = ( target.parentNode as HTMLElement ).offsetWidth
-    const ranger_height = Number(config.dataset.height);
-    const ranger_width = Number(config.dataset.width);
+    const rangerHeight = Number(config.dataset.height);
+    const rangerWidth = Number(config.dataset.width);
     const orientation = config.dataset.orientation;
-    if (orientation == 'horizontal') {
-        return Math.round(((configMax - configMin) / (ranger_width - 12)) * (coord)) + configMin;
+    if (orientation === 'horizontal') {
+        return Math.round(((configMax - configMin) / (rangerWidth - 12)) * (coord)) + configMin;
     }
-    else if (orientation == 'vertical') {
-        return Math.round(((configMax - configMin) / ranger_height) * (coord)) + configMin;
+    else if (orientation === 'vertical') {
+        return Math.round(((configMax - configMin) / rangerHeight) * (coord)) + configMin;
     }
 }
 export function reValueTip(element) {
+    /* Изменение значения атрибута tip  при изменении min, max в панели */
     let parent = element.parentNode.parentNode;
     let zdslider = parent.querySelector('.zdslider');
     let input = parent.querySelector('.zdslider-panel__check-tip');
     input.checked = false;
     let buttons = zdslider.querySelectorAll('.ranger__button');
-    for (let elem of buttons) {
+    buttons.forEach((elem) => {
         elem.dataset.tip = '';
-    }
+    });
     hideTip(element);
 }
--- src/ts/tipToggler.ts
+++ src/ts/tipToggler.ts
@@ -1,53 +1,64 @@
 export function showTip ( elem: HTMLElement, orientation: string ) {
-  let parent: HTMLElement = elem.parentNode.parentNode.querySelector('.zdslider')
-  let parentRanger: HTMLElement = parent.querySelector('.ranger')
-  let buttons = parentRanger.querySelectorAll('.ranger__button')
-  for (let elem of buttons) {
-    if ( orientation == 'horizontal' ) {
-      elem.classList.add('ranger__button-tip')
-    } else if ( orientation == 'vertical' ) {
-      elem.classList.add('ranger-vert__button-tip')
+  let parent: HTMLElement = elem.parentNode.parentNode.querySelector('.zdslider');
+  let parentRanger: HTMLElement = parent.querySelector('.ranger');
+  let buttons = parentRanger.querySelectorAll('.ranger__button');
+
+  buttons.forEach((elem) => {
+
+    if ( orientation === 'horizontal' ) {
+      elem.classList.add('ranger__button-tip');
+    } else if ( orientation === 'vertical' ) {
+      elem.classList.add('ranger-vert__button-tip');
     }
-  }
+
+  });
+
 }
   
 export function hideTip ( elem: HTMLElement ) {
-  let parent: HTMLElement = elem.parentNode.parentNode.querySelector('.zdslider')
-  let tip: HTMLInputElement = elem.parentNode.querySelector('.zdslider-panel__check-tip')
-  tip.checked = false           /* Сбрасываю флаг надписи */
+  let parent: HTMLElement = elem.parentNode.parentNode.querySelector('.zdslider');
+  let tip: HTMLInputElement = elem.parentNode.querySelector('.zdslider-panel__check-tip');
+
+  /* Сбрасываю флаг надписи */
+  tip.checked = false           
   let parentRanger: HTMLElement = parent.querySelector('.ranger')
   let buttons = parentRanger.querySelectorAll('.ranger__button')
-  for (let elem of buttons) {     /* Удаляю стили ярлыков */
+
+  /* Удаляю стили ярлыков */
+  buttons.forEach((elem) => {
     elem.classList.remove('ranger__button-tip')
     elem.classList.remove('ranger-vert__button-tip')
-  }
+  });
 }
 
 export function forTip ( target: HTMLElement, coord: number ) {
-  const config: HTMLElement = target.parentNode.parentNode.parentNode.querySelector('.zdslider-config')
-  const configMin: number = Number(config.dataset.min)
-  const configMax: number = Number(config.dataset.max)
-  // const ranger_height: number = ( target.parentNode as HTMLElement).offsetHeight
-  // const ranger_width: number = ( target.parentNode as HTMLElement ).offsetWidth
-  const ranger_height = Number ( config.dataset.height );
-  const ranger_width = Number ( config.dataset.width );
-  const orientation: string = config.dataset.orientation
-  if ( orientation == 'horizontal' ) {
-    return  Math.round(((configMax - configMin) / (ranger_width - 12) ) * (coord)) + configMin    
-  } else if ( orientation == 'vertical' ) {
-    return  Math.round(((configMax - configMin) / ranger_height ) * (coord)) + configMin
+  const config: HTMLElement = target.parentNode.parentNode.parentNode.querySelector('.zdslider-config');
+  const configMin: number = Number(config.dataset.min);
+  const configMax: number = Number(config.dataset.max);
+  const rangerHeight = Number ( config.dataset.height );
+  const rangerWidth = Number ( config.dataset.width );
+  const orientation: string = config.dataset.orientation;
+
+  if ( orientation === 'horizontal' ) {
+    return  Math.round(((configMax - configMin) / (rangerWidth - 12) ) * (coord)) + configMin    
+  } else if ( orientation === 'vertical' ) {
+    return  Math.round(((configMax - configMin) / rangerHeight ) * (coord)) + configMin
   } 
+
 }
 
-export function reValueTip ( element: HTMLElement ) {     /* Изменение значения атрибута tip  при изменении min, max в панели */
-  let parent = element.parentNode.parentNode as HTMLElement
-  let zdslider: HTMLElement = parent.querySelector('.zdslider');
-  let input = parent.querySelector('.zdslider-panel__check-tip') as HTMLInputElement
-  input.checked = false
+export function reValueTip ( element: HTMLElement ) {     
 
+  /* Изменение значения атрибута tip  при изменении min, max в панели */
+  let parent = element.parentNode.parentNode as HTMLElement;
+  let zdslider: HTMLElement = parent.querySelector('.zdslider');
+  let input = parent.querySelector('.zdslider-panel__check-tip') as HTMLInputElement;
+  input.checked = false;
   let buttons = zdslider.querySelectorAll('.ranger__button')
-  for ( let elem of buttons ) {
-    ( elem as HTMLElement ).dataset.tip = ''
-  }
-  hideTip( element )
+
+  buttons.forEach((elem) => {
+    ( elem as HTMLElement ).dataset.tip = '';
+  });
+
+  hideTip( element );
 }
